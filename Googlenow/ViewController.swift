fbc7f0fcdfc834536aa0ff2958e00c08da9bc694
vsirimalle <vinod.sirimalle@gmail.com>
2015-01-20 16:56:08 -0800

    Initial Commit

---
+++ Googlenow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Googlenow
+//
+//  Created by Vinod Sirimalle on 1/20/15.
+//  Copyright (c) 2015 vs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
